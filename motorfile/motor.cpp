6a24eb9e9821c2fdc1166bbdda99d2fab7d6472a
wooyong-kim <wooyong2223@naver.com>
2021-07-22 16:33:21 +0900

    모터

--- motorfile/motor.cpp
+++ motorfile/motor.cpp
@@ -1,21 +1,62 @@
 #include <stdio.h>
+#include <stdlib.h>	// exit 사용
 #include <wiringPi.h>
+#include <softPwm.h>
+#include <signal.h>	// signal 인터럽트
 
-#define MOTOR1 28	// GPIO20
-#define MOTOR2 29	// GPIO21
+#define MOTOR1A 24	// 35
+#define MOTOR2A 23	// 33
+#define ENA 25 //37
+
+#define MOTOR1B 22	//31
+#define MOTOR2B 21	//29
+#define ENB 30 // 27
+
+void init()
+{
+	digitalWrite(MOTOR1A, LOW);
+	digitalWrite(MOTOR2A, LOW);
+	digitalWrite(MOTOR1B, LOW);
+	digitalWrite(MOTOR2B, LOW);
+}
+
+void INThandler(int sig)
+{
+	init();
+	exit(0);
+}
 
 int main(void)
 {
 	if(wiringPiSetup() == -1) return 1;
+	signal(SIGINT, INThandler);	// ctrl + z
 	
-	pinMode(MOTOR1, OUTPUT);
-	pinMode(MOTOR2, OUTPUT);
+	pinMode(MOTOR1A, OUTPUT);
+	pinMode(MOTOR2A, OUTPUT);
+	pinMode(MOTOR1B, OUTPUT);
+	pinMode(MOTOR2B, OUTPUT);
 	
-	for(;;)
+	softPwmCreate(ENA, 0, 200);
+	softPwmCreate(ENB, 0, 200);
+	
+	while(1)
 	{
-		digitalWrite(MOTOR1, 1);
-		digitalWrite(MOTOR2, 0);
+		digitalWrite(MOTOR1A, 1);
+		digitalWrite(MOTOR2A, 0);
+		softPwmWrite(ENA, 180);
+		digitalWrite(MOTOR1B, 1);
+		digitalWrite(MOTOR2B, 0);
+		softPwmWrite(ENB, 180);
+		delay(1000);
+		digitalWrite(MOTOR1A, 0);
+		digitalWrite(MOTOR2A, 1);
+		softPwmWrite(ENA, 100);
+		digitalWrite(MOTOR1B, 0);
+		digitalWrite(MOTOR2B, 1);
+		softPwmWrite(ENB, 200);
+		delay(1000);
 	}
+	
 	
 	return 0;
 }
