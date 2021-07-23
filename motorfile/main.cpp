bb0e0a46d42c29b1650ee4c69f83dc7cff7f449d
wooyong-kim <wooyong2223@naver.com>
2021-07-23 18:07:25 +0900

    모터 함수 세분화

---
+++ motorfile/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdlib.h>	// exit 사용
+#include <wiringPi.h>
+#include <softPwm.h>
+#include <signal.h>	// signal 인터럽트
+#include "motor.h"
+
+using namespace std;
+
+int main(void)
+{
+	if(wiringPiSetup() == -1) return 1;
+	signal(SIGINT, INThandler);	// ctrl + z 종료
+
+	motor_value();
+	
+	while(1)
+	{
+		motor_straight(200);
+		delay(1000);
+	}
+	
+	return 0;
+}
