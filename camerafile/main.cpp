3ae3f906d84366ca703e4d40cd58271e1a7e99d2
wooyong-kim <wooyong2223@naver.com>
2021-07-31 16:16:35 +0900

    탭

--- camerafile/main.cpp
+++ camerafile/main.cpp
@@ -10,62 +10,55 @@ using namespace std;
 
 int main()
 {
-   VideoCapture cap(-1);
-   //VideoCapture cap("unknown.png");
-   
-   if(!cap.isOpened())
-      cout << "첫번째 카메라를 열 수 없습니다." << endl;
-      
-   if(wiringPiSetup() == -1) return 1;
-   signal(SIGINT, INThandler);	// ctrl + z 종료
-      
-   //double fps = cap.get(CAP_PROP_FPS);
-   int width = cap.get(CAP_PROP_FRAME_WIDTH);
-   int height = cap.get(CAP_PROP_FRAME_HEIGHT);
-   
-   motor_value();
-   //namedWindow("cam", 1);
-   //Color_createTrackbar();
-
-   while(1)
-   {
-      Mat frame, black_img, can, lines, colorselect;
-      Mat Result, frame2;
+	VideoCapture cap(-1);
+	//VideoCapture cap("unknown.png");
 	
-      cap >> frame;
-      flip(frame, frame, -1); // 카메라 상하 반전
-      
-      resize(frame, frame2, Size(width, height));
-      
-      can = color(frame2, width, height);
-      
-      frame2.copyTo(colorselect);
-      
-      Result = Point_img(can, width, height);
-      
-      frame2.copyTo(lines);
-      vector<Vec4i> linesP;
-      HoughLinesP(Result, linesP, 2, CV_PI/180, 15, 100, 100);
-      float slope = draw_line(lines, linesP, width, height);
-      //printf("%f\n", slope);
-      //delay(100);
-      
-      if(slope < 1 && slope > -1)
-      {
-		  motor_straight(200);
-		  }
-		  else
-		  {
-			  motor_init();
-		  }
-      
-      imshow("cam1", lines);
-      imshow("cam2", can);
-
-      if(waitKey(1) == 27) break;   //esc입력시 종료
-   }
-   
-   destroyAllWindows();
-
-   return 0;
+	if(!cap.isOpened())
+	cout << "첫번째 카메라를 열 수 없습니다." << endl;
+	if(wiringPiSetup() == -1) return 1;
+	signal(SIGINT, INThandler);	// ctrl + z 종료
+	
+	//double fps = cap.get(CAP_PROP_FPS);
+	int width = cap.get(CAP_PROP_FRAME_WIDTH);
+	int height = cap.get(CAP_PROP_FRAME_HEIGHT);
+	
+	motor_value();
+	//namedWindow("cam", 1);
+	//Color_createTrackbar();
+	while(1)
+	{
+		Mat frame, black_img, can, lines, colorselect;
+		Mat Result, frame2;
+		
+		cap >> frame;
+		flip(frame, frame, -1); // 카메라 상하 반전
+		resize(frame, frame2, Size(width, height));
+		
+		can = color(frame2, width, height);
+		
+		frame2.copyTo(colorselect);
+		
+		Result = Point_img(can, width, height);
+		
+		frame2.copyTo(lines);
+		vector<Vec4i> linesP;
+		HoughLinesP(Result, linesP, 2, CV_PI/180, 15, 100, 100);
+		float slope = draw_line(lines, linesP, width, height);
+		//printf("%f\n", slope);
+		//delay(100);
+		if(slope < 1 && slope > -1)
+		{
+			motor_straight(200);
+		}
+		else
+		{
+			motor_init();
+		}
+		
+		imshow("cam1", lines);
+		imshow("cam2", can);
+		if(waitKey(1) == 27) break;   //esc입력시 종료
+	}
+	destroyAllWindows();
+	return 0;
 }
